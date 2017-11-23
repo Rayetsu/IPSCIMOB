370ad6309ecead459c2685756ff7095efe0bfbd0
Francisco <francisco.alves@estudantes.ips.pt>
2017-11-23 20:02:43 +0000

    Classe Notificacao

---
+++ IPSCIMOB/Models/Notificacao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace IPSCIMOB.Models
+{
+    public class Notificacao
+    {
+        public Notificacao(string email, string assunto, string conteudo)
+        {
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.To.Add(email);
+            mail.From = new MailAddress("projcimob@gmail.com", "IPS-CIMOB", System.Text.Encoding.UTF8);
+            mail.Subject = assunto;
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = conteudo;
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("projcimob@gmail.com", "proj1234");
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Exception ex2 = ex;
+                string errorMessage = string.Empty;
+                while (ex2 != null)
+                {
+                    errorMessage += ex2.ToString();
+                    ex2 = ex2.InnerException;
+                }
+            }
+        }
+    }
+}
