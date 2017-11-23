9eb40ba2c879571b4a8d0db398949d4460afe968
Rayetsu <gabriel25asilva@gmail.com>
2017-11-23 12:43:00 +0000

    Add project files.

---
+++ IPSCIMOB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IPSCIMOB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
