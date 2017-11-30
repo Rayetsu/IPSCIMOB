f60dd25d02c17836fb71897960be51318dff5513
Rayetsu <gabriel25asilva@gmail.com>
2017-11-30 13:27:33 +0000

    DbContext

--- IPSCIMOB/Data/ApplicationDbContext.cs
+++ IPSCIMOB/Data/ApplicationDbContext.cs
@@ -21,6 +21,9 @@ namespace IPSCIMOB.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            
+            
         }
 
         public DbSet<IPSCIMOB.Models.Utilizador> Utilizador { get; set; }
