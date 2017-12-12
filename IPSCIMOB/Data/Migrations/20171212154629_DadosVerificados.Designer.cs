7a3ddd13ac5555b4ffc0fdfc0593db952da299dd
Rayetsu <gabriel25asilva@gmail.com>
2017-12-12 15:49:01 +0000

    adicionado atributo ao ApplicationUser

--- IPSCIMOB/Data/Migrations/20171212154629_DadosVerificados.Designer.cs
+++ IPSCIMOB/Data/Migrations/20171212154629_DadosVerificados.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace IPSCIMOB.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171212154629_DadosVerificados")]
+    partial class DadosVerificados
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
