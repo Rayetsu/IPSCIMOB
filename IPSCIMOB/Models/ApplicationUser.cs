bb74f37c915de6c0b316d236c12063ee7e234afc
Rayetsu <gabriel25asilva@gmail.com>
2017-12-14 14:41:58 +0000

    Alterações visuais

--- IPSCIMOB/Models/ApplicationUser.cs
+++ IPSCIMOB/Models/ApplicationUser.cs
@@ -13,12 +13,15 @@ namespace IPSCIMOB.Models
         [StringLength(50)]
         public string Nome { get; set; }
 
+        [Display(Name = "Número Interno")]
         public int NumeroInterno { get; set; }
 
+        [Display(Name = "Número do BI")]
         public int NumeroDoBI { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataDeNascimento { get; set; }
 
         [StringLength(50)]
@@ -26,10 +29,13 @@ namespace IPSCIMOB.Models
 
         public int Telefone { get; set; }
 
+        [Display(Name = "Partilha Mobilidade")]
         public bool PartilhaMobilidade { get; set; }
 
+        [Display(Name = "Funcionário")]
         public bool IsFuncionario { get; set; }
 
+        [Display(Name = "Dados Verificados")]
         public bool IsDadosVerificados { get; set; }
     }
 }
