e6862c0bfc19af4b43223991e16bb56c84466487
Rayetsu <gabriel25asilva@gmail.com>
2017-12-14 15:52:11 +0000

    Verificar dados utilizador e escolher processo de mobilidade

--- IPSCIMOB/Controllers/HomeController.cs
+++ IPSCIMOB/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IPSCIMOB.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IPSCIMOB.Controllers
 {
@@ -27,6 +28,43 @@ namespace IPSCIMOB.Controllers
             //ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        public IActionResult CIMOB()
+        {
+
+            return View();
+        }
+
+        public IActionResult SelecionarMobilidade()
+        {
+
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            items.Add(new SelectListItem { Text = "Vasco da Gama", Value = "0" });
+
+            items.Add(new SelectListItem { Text = "Politécnico de Macau", Value = "1" });
+
+            items.Add(new SelectListItem { Text = "Erasmus+", Value = "2", Selected = true });
+
+            items.Add(new SelectListItem { Text = "Erasmus+ Formação/Trabalho", Value = "3" });
+
+            items.Add(new SelectListItem { Text = "Santander Universidades", Value = "4" });
+
+            items.Add(new SelectListItem { Text = "Santander Missões de Ensino e Formação", Value = "5" });
+
+            ViewBag.TipoDeMobilidade = items;
+
+            return View();
+        }
+
+        public ViewResult MobilidadeEscolhida(string TipoDeMobilidade)
+        {
+
+            ViewBag.messageString = TipoDeMobilidade;
+
+            return View("Information");
+
         }
 
         public IActionResult ProgramaVascoDaGama()
