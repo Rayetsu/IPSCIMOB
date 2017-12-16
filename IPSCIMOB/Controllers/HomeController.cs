f8f14dda5e2bd431c336632c0bf65070b421c598
Rayetsu <gabriel25asilva@gmail.com>
2017-12-16 23:54:24 +0000

    Escolher o processo de mobilidade: codigo nao estava a fazer o que se pretendia, corrigido

--- IPSCIMOB/Controllers/HomeController.cs
+++ IPSCIMOB/Controllers/HomeController.cs
@@ -28,33 +28,33 @@ namespace IPSCIMOB.Controllers
         public IActionResult SelecionarMobilidade()
         {
 
-            List<SelectListItem> items = new List<SelectListItem>();
+            //List<SelectListItem> items = new List<SelectListItem>();
 
-            items.Add(new SelectListItem { Text = "Vasco da Gama", Value = "0" });
+            //items.Add(new SelectListItem { Text = "Vasco da Gama", Value = "0" });
 
-            items.Add(new SelectListItem { Text = "Politécnico de Macau", Value = "1" });
+            //items.Add(new SelectListItem { Text = "Politécnico de Macau", Value = "1" });
 
-            items.Add(new SelectListItem { Text = "Erasmus+", Value = "2", Selected = true });
+            //items.Add(new SelectListItem { Text = "Erasmus+", Value = "2", Selected = true });
 
-            items.Add(new SelectListItem { Text = "Erasmus+ Formação/Trabalho", Value = "3" });
+            //items.Add(new SelectListItem { Text = "Erasmus+ Formação/Trabalho", Value = "3" });
 
-            items.Add(new SelectListItem { Text = "Santander Universidades", Value = "4" });
+            //items.Add(new SelectListItem { Text = "Santander Universidades", Value = "4" });
 
-            items.Add(new SelectListItem { Text = "Santander Missões de Ensino e Formação", Value = "5" });
+            //items.Add(new SelectListItem { Text = "Santander Missões de Ensino e Formação", Value = "5" });
 
-            ViewBag.TipoDeMobilidade = items;
+            //ViewBag.TipoDeMobilidade = items;
 
             return View();
         }
 
-        public ViewResult MobilidadeEscolhida(string TipoDeMobilidade)
-        {
+        //public ViewResult MobilidadeEscolhida(string TipoDeMobilidade)
+        //{
 
-            ViewBag.messageString = TipoDeMobilidade;
+        //    ViewBag.messageString = TipoDeMobilidade;
 
-            return View("Information");
+        //    return View("Information");
 
-        }
+        //}
 
         [Authorize(Roles = "Aluno, Funcionário")]
         public IActionResult Sugestoes()
@@ -63,6 +63,41 @@ namespace IPSCIMOB.Controllers
             return View();
         }
 
+        public IActionResult VascoDaGama()
+        {
+
+            return View();
+        }
+
+        public IActionResult PolitecnicoDeMacau()
+        {
+
+            return View();
+        }
+
+        public IActionResult Erasmus()
+        {
+
+            return View();
+        }
+
+        public IActionResult ErasmusFormacaoTrabalho()
+        {
+
+            return View();
+        }
+
+        public IActionResult SantanderUniversidades()
+        {
+
+            return View();
+        }
+
+        public IActionResult SantanderMissoes()
+        {
+
+            return View();
+        }
 
         public IActionResult Error()
         {
