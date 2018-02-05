8ede91aa5ef8194004051a57020f5d6a8bba6914
Francisco <francisco.alves@estudantes.ips.pt>
2018-02-05 16:21:11 +0000

    required sugestao

--- IPSCIMOB/Models/Sugestao.cs
+++ IPSCIMOB/Models/Sugestao.cs
@@ -10,7 +10,9 @@ namespace IPSCIMOB.Models
     {
         [Key]
         public int SugestaoID { get; set; }
+        [Required]
         public String EmailUtilizador { get; set; }
+        [Required]
         public String TextoSugestao { get; set; }
     }
 }
