432c73593fb4c71ae75927ee05cc994c2d6c1048
Tiago Branco <someone1995@hotmail.com>
2018-02-11 21:20:07 +0000

    update testes tiago

--- XUnitTestProject1/TestesForeingStudentsController.cs
+++ XUnitTestProject1/TestesForeingStudentsController.cs
@@ -62,5 +62,89 @@ namespace XUnitTestProject1
             Assert.Equal(student, res);
             Assert.Equal("Joana Silva", student.Nome);
         }
+
+        [Fact]
+        public async Task TestDeleteForeingStudents()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            // Arrange
+            var controller = new ForeignStudentsController(_dbContext);
+
+            // Act
+            var student = new ForeignStudents()
+            {
+                Nome = "Joana Silva",
+                Nacionalidade = "USA",
+                Email = "jusilva@gmail.com",
+                DataDeNascimento = new DateTime(1997, 12, 29),
+                EscolaIPSECurso = "EST, EI",
+                Morada = "Main Street Avenue",
+                NumeroDaPorta = 34,
+                Andar = "2",
+                Cidade = "New York",
+                Distrito = "distric1",
+                CodigoPostal = "45755-7856",
+                Universidade = "NY University",
+                IsBolseiro = false,
+                IsFuncionario = false,
+                IsDadosVerificados = true
+            };
+            await controller.Create(student);
+
+            await controller.DeleteConfirmed(student.Id);
+
+            var res = await _dbContext.ForeignStudents.SingleOrDefaultAsync(m => m.Id == student.Id);
+            Assert.Null(res);
+        }
+
+        [Fact]
+        public async Task TestEditarForeingStudent()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            // Arrange
+            var controller = new ForeignStudentsController(_dbContext);
+
+            // Act
+            var student = new ForeignStudents()
+            {
+                Nome = "Ruan Bandera",
+                Nacionalidade = "Americano",
+                Email = "ruanzinho@gmail.com",
+                DataDeNascimento = new DateTime(1997, 12, 29),
+                EscolaIPSECurso = "EST, EI",
+                Morada = "Main Street Avenue",
+                NumeroDaPorta = 34,
+                Andar = "2",
+                Cidade = "New York",
+                Distrito = "distric1",
+                CodigoPostal = "45755-7856",
+                Universidade = "NY University",
+                IsBolseiro = false,
+                IsFuncionario = false,
+                IsDadosVerificados = true
+            };
+            await controller.Create(student);
+
+            student.Nome = "Ruan De La Bandera";
+            student.Email = "mexicanBoy@email.com";
+            student.IsBolseiro = true;
+            student.Nacionalidade = "Mexicano";
+            await controller.Edit(student.Id, student);
+
+            var res = await _dbContext.ForeignStudents.SingleOrDefaultAsync(m => m.Id == student.Id);
+            Assert.Equal(res.Nome, student.Nome);
+            Assert.Equal("Ruan De La Bandera", res.Nome);
+            Assert.Equal("mexicanBoy@email.com", res.Email);
+            Assert.True(res.IsBolseiro);
+            Assert.Equal("Mexicano", res.Nacionalidade);
+        }
+
+
     }
 }
--- XUnitTestProject1/TestesInstituicoesController.cs
+++ XUnitTestProject1/TestesInstituicoesController.cs
@@ -25,50 +25,91 @@ namespace XUnitTestProject1
         private ApplicationDbContext _dbContext;
 
         [Fact]
-        public async Task TestCreateInsitituicaoParceira()
+        public async Task TestCreateInstituicaoParceira()
         {
             optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             optionsBuilder.UseInMemoryDatabase();
             _dbContext = new ApplicationDbContext(optionsBuilder.Options);
-                       
+
             // Arrange
             var controller = new InstituicaoParceiraController(_dbContext);
-        
+
             // Act
             var i = new InstituicaoParceiraModel()
-            {               
+            {
                 Nome = "Instituto Politecnico de Sines",
                 Cidade = "Sines",
                 ProgramaNome = "Erasmus+ ALUNOS"
             };
             var result = await controller.Create(i);
 
-            var res = await _dbContext.InstituicaoParceiraModel.SingleOrDefaultAsync(m => m.InstituicaoParceiraID == i.InstituicaoParceiraID);
+            var res = await _dbContext.InstituicaoParceiraModel.SingleOrDefaultAsync
+                (m => m.InstituicaoParceiraID == i.InstituicaoParceiraID);
             Assert.Equal(i, res);
             Assert.Equal("Instituto Politecnico de Sines", i.Nome);
         }
 
-        //public async Task TestDeleteInsitituicaoParceira()
-        //{
-        //    optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        //    optionsBuilder.UseInMemoryDatabase();
-        //    _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+        [Fact]
+        public async Task TestDeleteInsitituicaoParceira()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            _dbContext = new ApplicationDbContext(optionsBuilder.Options);
 
-        //    Arrange
-        //   var controller = new InstituicaoParceiraController(_dbContext);
+            // Arrange
+            var controller = new InstituicaoParceiraController(_dbContext);
 
-        //    Act
-        //   var i = new InstituicaoParceiraModel()
-        //   {
-        //       Nome = "Instituto Politecnico de Braga",
-        //       Cidade = "Braga",
-        //       ProgramaNome = "Erasmus+ FUNCIONARIOS"
-        //   };
-        //    var result = await controller.Delete(i.InstituicaoParceiraID);
+            // Act
+            var i = new InstituicaoParceiraModel()
+            {
+                Nome = "Instituto Politecnico para Eliminar",
+                Pais = "DeleteCountry",
+                Cidade = "DeleteCity",
+                ProgramaNome = "Erasmus+ FUNCIONARIOS"
+            };
+            await controller.Create(i);
+            await controller.DeleteConfirmed(i.InstituicaoParceiraID);
 
-        //    var res = await _dbContext.InstituicaoParceiraModel.SingleOrDefaultAsync(m => m.InstituicaoParceiraID == i.InstituicaoParceiraID);
-        //    Assert.Contains();
-        //    Assert.Equal("Instituto Politecnico de Sines", i.Nome);
-        //}
+            var res = await _dbContext.InstituicaoParceiraModel.SingleOrDefaultAsync(m => m.InstituicaoParceiraID == i.InstituicaoParceiraID);
+            Assert.Null(res);
+        }
+
+
+        [Fact]
+        public async Task TestEditarInsitituicaoParceira()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            // Arrange
+            var controller = new InstituicaoParceiraController(_dbContext);
+
+            // Act
+            var i = new InstituicaoParceiraModel()
+            {
+                Nome = "Instituto Politecnico para Editar",
+                Pais = "França",
+                Cidade = "Nantes",
+                ProgramaNome = "Erasmus+ FUNCIONARIOS"
+            };
+            await controller.Create(i);
+
+            i.Nome = "Instituto Politecnico Editado";
+            i.Pais = "Alemanha";
+            i.Cidade = "Frankfurt";
+            i.ProgramaNome = "Erasmus+ ALUNOS";
+            await controller.Edit(i.InstituicaoParceiraID, i);
+
+            var res = await _dbContext.InstituicaoParceiraModel.SingleOrDefaultAsync(m => m.InstituicaoParceiraID == i.InstituicaoParceiraID);
+            Assert.Equal(res.Nome, i.Nome);
+            Assert.Equal("Instituto Politecnico Editado", res.Nome);
+            Assert.Equal("Alemanha", res.Pais);
+            Assert.Equal("Frankfurt", res.Cidade);
+            Assert.Equal("Erasmus+ ALUNOS", res.ProgramaNome);
+        }
+
+
     }
+
 }
